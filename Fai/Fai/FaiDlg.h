d7fe5d7ebdc18c2940c066c8f6406ec7093e11df
Haiezan <807544235@qq.com>
2021-07-15 17:58:45 +0800

    稳定系数计算程序

---
+++ Fai/Fai/FaiDlg.h
@@ -0,0 +1,47 @@
+﻿
+// FaiDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CFaiDlg 对话框
+class CFaiDlg : public CDialogEx
+{
+// 构造
+public:
+	CFaiDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FAI_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CComboBox m_cComboxCatg;
+	int m_iCatg;
+	// 长细比
+	float m_fCxb;
+	// 屈服强度
+	float m_fFy;
+
+	float m_fFai;
+	CString m_sMsg;
+	afx_msg void OnBnClickedButton1();
+	// 弹性模量
+	float m_fE;
+};
---
+++ Fai/Fai/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Fai.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FAI_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDC_COMBO_CATG                  1000
+#define IDC_EDIT_CXB                    1001
+#define IDC_EDIT_FY                     1002
+#define IDC_EDIT_MSG                    1003
+#define IDC_BUTTON1                     1004
+#define IDC_EDIT_FY2                    1005
+#define IDC_EDIT_E                      1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

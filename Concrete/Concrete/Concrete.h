17e013e99e3648ac9021f1e4fc24b89d4bdb5b5b
Haiezan <807544235@qq.com>
2021-09-02 17:06:44 +0800

    增加混凝土本构项目

---
+++ Concrete/Concrete/Concrete.h
@@ -0,0 +1,32 @@
+﻿
+// Concrete.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CConcreteApp:
+// 有关此类的实现，请参阅 Concrete.cpp
+//
+
+class CConcreteApp : public CWinApp
+{
+public:
+	CConcreteApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CConcreteApp theApp;

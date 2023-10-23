d1cec863c8abb0a829d23a6093143c9bf14916b5
jiasu <807544235@qq.com>
2023-10-23 21:30:30 +0800

    Update DlgABA.cpp

--- ABAISO/ABAISO/DlgABA.cpp
+++ ABAISO/ABAISO/DlgABA.cpp
@@ -65,7 +65,7 @@ void CDlgABA::SetData()
 	m_cList.AddString(L"Elasticity");
 
 	if (m_ISO.IsPlastic()) m_cList.AddString(L"Plasticity");
-	if (m_ISO.IsDamping()) m_cList.AddString(L"Damping");
+	//if (m_ISO.IsDamping()) m_cList.AddString(L"Damping");
 	m_cList.SetCurSel(0);
 }
 
@@ -88,8 +88,8 @@ void CDlgABA::OnLbnSelchangeList1()
 
 		//建立表头，即为每列起名字。注意列是从1开始
 		m_cListData.InsertColumn(1, L"", LVCFMT_CENTER, 30);
-		m_cListData.InsertColumn(2, L"D11", LVCFMT_CENTER, 100);
-		m_cListData.InsertColumn(3, L"D22", LVCFMT_CENTER, 100);
+		m_cListData.InsertColumn(2, L"D11", LVCFMT_CENTER, 50);
+		m_cListData.InsertColumn(3, L"D22", LVCFMT_CENTER, 50);
 		m_cListData.InsertColumn(4, L"D33", LVCFMT_CENTER, 100);
 
 		//先创建行，再为改行每列赋值。注意列是从0开始
@@ -110,30 +110,34 @@ void CDlgABA::OnLbnSelchangeList1()
 		m_bU[0] = m_ISO.m_bUN[1];
 		m_bU[1] = m_ISO.m_bUN[2];
 
-		m_bHard1 = TRUE;
-		m_bHard2 = FALSE;
+		m_bHard1 = FALSE;
+		m_bHard2 = TRUE;
+
+		float K0 = m_ISO.m_fNL[2]; //初始刚度
+		float Fb0 = m_ISO.m_fNL[3]; //屈服力
+		float B0 = m_ISO.m_fNL[4]; //屈服后刚度比
+
+		float Fb1 = (1.0 - Fb0 / K0) * K0 * B0 + Fb0; //应变为1时对应内力
+		float PlasticStrain = 1.f - Fb1 / K0; //应变为1时对应塑性应变
 
 		//建立表头，即为每列起名字。注意列是从1开始
 		m_cListData.InsertColumn(1, L"", LVCFMT_CENTER, 30);
 		m_cListData.InsertColumn(2, L"Yield Force//Moment", LVCFMT_CENTER, 100);
 		m_cListData.InsertColumn(3, L"Plastic Motion", LVCFMT_CENTER, 100);
-		m_cListData.InsertColumn(4, L"Rate", LVCFMT_CENTER, 100);
+		//m_cListData.InsertColumn(4, L"Rate", LVCFMT_CENTER, 100);
 
 		//先创建行，再为改行每列赋值。注意列是从0开始
 		CString str;
 		m_cListData.InsertItem(0, L"1");
-		//m_cListData.SetItemText(0, 0, L"1");
-		str.Format(L"%g", m_ISO.m_fK[1]);
+		str.Format(L"%g", Fb0);
 		m_cListData.SetItemText(0, 1, str);
-		str.Format(L"%g", m_ISO.m_fK[2]);
-		m_cListData.SetItemText(0, 2, str);
-		str.Format(L"%g", m_ISO.m_fK[0]);
-		m_cListData.SetItemText(0, 3, str);
+		m_cListData.SetItemText(0, 2, L"0.0");
 
+		m_cListData.InsertItem(1, L"2");
+		str.Format(L"%g", Fb1);
+		m_cListData.SetItemText(1, 1, str);
+		str.Format(L"%g", PlasticStrain);
+		m_cListData.SetItemText(1, 2, str);
 	}
-	
-
-	
-
 	UpdateData(FALSE);
 }
